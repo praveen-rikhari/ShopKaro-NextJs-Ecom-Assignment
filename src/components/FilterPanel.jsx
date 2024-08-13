6e1f0856a5b7b661d659c8235367390996506249
praveen-rikhari <praveenrikhari45@gmail.com>
2024-08-13 22:08:49 +0530

    Adding filter and product component

---
+++ src/components/FilterPanel.jsx
@@ -0,0 +1,12 @@
+const FilterPanel = () => {
+    return (
+        <div className="filter-panel">
+            <h3>Filters</h3>
+            <div className="filter-option">Category</div>
+            <div className="filter-option">Price Range</div>
+            <div className="filter-option">Size</div>
+        </div>
+    );
+};
+
+export default FilterPanel;
